f93ddc8f793dbeb8a3f8e72dca2745f175fe669b
zyia238 <q278767942@gmail.com>
2022-12-07 13:25:28 +1300

    addToCart function finished

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import UserProvider from './contexts/user.context'
+import CartProvider from './contexts/cart.context'
 import ProductsProvider from './contexts/products.context'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +15,9 @@ root.render(
     <Router>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </Router>
